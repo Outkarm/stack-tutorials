3431291526b55cf7981977877b475c56d10c039f
Outkarm <jkpordje@gmail.com>
2023-09-05 20:39:21 +0000

    updated queue with basic queue functions

--- queueJavaScriptPractice.js
+++ queueJavaScriptPractice.js
@@ -3,9 +3,38 @@ class Queue {
     this.items = [];
   }
   enqueue(e) {
-    this.item.push(e);
+    this.items.push(e);
   }
   dequeue() {
-    return this;
+    return this.items.shift();
+  }
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[0];
+    } else {
+      return null;
+    }
+  }
+  size() {
+    return this.items.length;
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  print() {
+    console.log(this.items);
   }
 }
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue(10);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+queue.print();
+queue.enqueue(20);
+queue.dequeue();
+console.log(queue.size());
+console.log(queue.peek());
+console.log(queue.isEmpty());
+queue.print();
